29fc8d0fae9019f1b00e239f3f9c2a9c5cbc83bc
Dan Barthell <dbarthell@Dans-MacBook-Pro.local>
2018-04-30 23:28:06 -0500

    moved donna.js into quiz.js

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -1,80 +1,88 @@
-function startQuiz() {	
-    // When the button is clicked the quiz starts and the first question is shown. All other questions are hidden. Do one question at a time.  
-    document.getElementById("intro").style.display = "none";
-        document.getElementById("question1").style.display = "block";
-    }
-    
+$(document).ready(function () {
+    $('#California').hide();
+    $('#Italy').hide();
+    $('#Iceland').hide();
+    $('#Mexico').hide();
+
     document.getElementById("beginquiz").addEventListener("click", startQuiz);
     //event listener for "start quiz" button
-    var answerData = {		
+    var answerData = {
         //object stores quiz answers. a value of 1 is given to each location when an answer associated with the location is clicked. 
         "California": { score: 0 },
         "Bahamas": { score: 0 },
         "Italy": { score: 0 },
         "Iceland": { score: 0 }
     };
-    
-    var buttons = document.querySelectorAll(".button");	
+
+    var buttons = document.querySelectorAll(".button");
     // for the button elements
-        
-    for (var i = 0 ; i < buttons.length ; i++) {	
-        buttons[i].onclick = buttonClicked;	
-         //if a button is clicked the buttonClicked function is called
-        }
-    
-    function buttonClicked(e) {	
+
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].onclick = buttonClicked;
+        //if a button is clicked the buttonClicked function is called
+    }
+
+    function startQuiz() {
+        // When the button is clicked the quiz starts and the first question is shown. All other questions are hidden. Do one question at a time.  
+        document.getElementById("intro").style.display = "none";
+        document.getElementById("question1").style.display = "block";
+    }
+
+    function buttonClicked(e) {
         // button click to answer the questions
-    
-        var target = e.target; 	
-        
-        console.log(target); 
-    
+        var target = e.target;
+
+        console.log(target);
+
         var selectedType = target.dataset.score;
         // Get the value score from user answer	
-        console.log(selectedType);	
+        console.log(selectedType);
         answerData[selectedType].score++;	// add 1 to user score
-        
-        this.parentElement.style.display = "none";		
+
+        this.parentElement.style.display = "none";
         // Hides questions in the div
-        var nextQuestion = this.parentElement.dataset.next;		
+        var nextQuestion = this.parentElement.dataset.next;
         // shows next question in teh div when previous question is answered
-        
-        if (nextQuestion != results) { 
+
+        if (nextQuestion != results) {
             // Display the next question if the nextQuestion is not equal to "result". This lets the quiz keep going until the result ends the quiz. 
-            document.getElementById(nextQuestion).style.display = "block"}
-            
-            else if (nextQuestion == results) {	
-                // If the nextQuestion is equal to result, then show the result. 
-    
-            document.getElementById(nextQuestion).style.display = "block"}	
-            //block allows combined statements. This allows questions and results to be displayed. 
-            
-            document.getElementById("finalResulthere").innerHTML = "<p>You should go to " + finalResults() +"!</p>";	
-                 // Post the final results to the appropriate result div. Shows user where they should go based on answers.
-            
-            if (document.getElementById("results").style.display == "block") { 
+            document.getElementById(nextQuestion).style.display = "block"
+        }
+
+        else if (nextQuestion == results) {
+            // If the nextQuestion is equal to result, then show the result. 
+
+            document.getElementById(nextQuestion).style.display = "block"
+        }
+        //block allows combined statements. This allows questions and results to be displayed. 
+
+        document.getElementById("finalResulthere").innerHTML = "<h1>You should go to " + finalResults() + "!</h1>";
+        // Post the final results to the appropriate result div. Shows user where they should go based on answers.
+
+        if (document.getElementById("results").style.display == "block") {
             //these are the results for the quiz.
-                
-                 if (finalResults == "California") {
-                             
-                 }
-                 else if (finalResults == "Bahamas") {
-                     
-                 }
-                 else if (finalResults == "Italy") {
-            
-                 }
-                 else if (finalResults == "Iceland") {
-                 
-                 }
-              }
+
+            if (finalResults == "California") {
+
             }
-    
-    function finalResults() {	
+            else if (finalResults == "the Bahamas") {
+
+            }
+            else if (finalResults == "Italy") {
+
+            }
+            else if (finalResults == "Iceland") {
+
+            }
+        }
+    }
+
+    function finalResults() {
         // Get the values of the answers object 
-    
-        var theAnswer = Object.keys(answerData).reduce(function(a, b){ return answerData[a].score > answerData[b].score ? a : b });	
+
+        var theAnswer = Object.keys(answerData).reduce(function (a, b) { return answerData[a].score > answerData[b].score ? a : b });
         //search for how to score personality test, rather than a typical quiz. 
         return theAnswer;
     }
-    
+
+});
